3077934097f73423ffb80870da26e540d49189bc
fivehills <40301946+fivehills@users.noreply.github.com>
2023-02-13 12:16:14 +0100

    Add files via upload

---
+++ orangejuice_ge.js
@@ -0,0 +1,75 @@
+{
+    "root": [
+        {
+            "id": 0,
+            "parent": -1,
+            "text": "ROOT",
+            "relation": "null"
+        },
+        {
+            "id": 1,
+            "parent": 3,
+            "text": "Orangensaft ist ein Fruchtsaft, der durch Auspressen von Orangen hergestellt wird. <S>",
+            "relation": "Conjunction"
+        },
+        {
+            "id": 2,
+            "parent": 1,
+            "text": "Umgangssprachlich wird er oft als O-Saft bezeichnet. <S>",
+            "relation": "Detail"
+        },
+        {
+            "id": 3,
+            "parent": 0,
+            "text": "Orangensaft ist der meistgetrunkene Saft der Welt,",
+            "relation": "Root"
+        },
+        {
+            "id": 4,
+            "parent": 3,
+            "text": "und Fruchtsaft aus Fruchtsaftkonzentrat. Orangen-Direktsaft wird im Ernteland (hauptsächlich Brasilien) nach dem Pressen durch Pasteurisation haltbar gemacht.  <S>",
+            "relation": "Cause"
+        },
+        {
+            "id": 5,
+            "parent": 3,
+            "text": "Unterschieden wird zwischen Direktsaft und Fruchtsaft aus Fruchtsaftkonzentrat. <S>": "Conjunction"
+        },
+        {
+            "id": 6,
+            "parent": 5,
+            "text": "Orangen-Direktsaft wird im Ernteland (hauptsächlich Brasilien) nach dem Pressen durch Pasteurisation haltbar gemacht. <S>",
+            "relation": "Detail"
+        },
+        {
+            "id": 7,
+            "parent": 8,
+            "text": "Befindet sich das Zielland in Übersee, beispielsweise Europa,",
+            "relation": "Condition"
+        },
+        {
+            "id": 8,
+            "parent": 9,
+            "text": "so wird er per Orangensafttanker transportiert und im Zielland von den Fruchtsaftherstellern abgefüllt.  <S>",
+            "relation": "Cause"
+        }
+  {
+            "id": 9,
+            "parent": 5,
+            "text": "Der Hauptteil des gewonnenen Orangensaftes wird noch im Produktionsland zu Orangensaftkonzentrat verarbeitet. <S>",
+            "relation": "Detail"
+        }
+  {
+            "id": 10,
+            "parent": 9,
+            "text": "Dazu werden dem Orangensaft unter Vakuum bei niedrigen Temperaturen zuerst die Aromen und anschließend das Wasser entzogen. <S> ",
+            "relation": "Purpose"
+        }
+  {
+            "id": 11,
+            "parent": 5,
+            "text": "Laut Fruchtsaft- und Erfrischungsgetränkeverordnung darf als Saft nur ein solches Getränk bezeichnet werden, das vollständig aus dem Fruchtsaft und dem Fruchtfleisch der entsprechenden Früchte stammt. <S> <P>",
+            "relation": "Cause"
+        }
+    ]
+}
